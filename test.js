22a4c76cebd876c2a4cc123434765198e8f5ad1b
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2023-07-25 19:14:26 +0530

    updates

--- test.js
+++ test.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/cat", (req, res) => {
+  res.send("Billi");
+});
+app.get("/dog", (req, res) => {
+  res.send("Kutta");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
